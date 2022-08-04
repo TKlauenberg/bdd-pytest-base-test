5963e5e51c7db322a611a173dc70449b9130e690
Tobias Klauenberg <toklaui@live.de>
2022-08-04 23:22:33 +0200

    add basic test configs

--- tests/stepdefinitions/test_pytest_bdd.py
+++ tests/stepdefinitions/test_pytest_bdd.py
@@ -1,14 +1,21 @@
 from pytest_bdd import scenario, given, when, then, parsers
 from testinfra import host
 
-@given("Klaus configured a basic Host")
-def klaus_configured_a_basic_host():
-    return "test"
+serviceConfigs = {
+    'basic': ['NetworkManager.service', 'sshd.service'],
+    'extra': ['NetworkManager.service', 'sshd.service', 'testservice']
+}
 
-@when("the host is provisioned")
+@given(parsers.parse('Klaus configured a {role} Host'), target_fixture='role')
+def klaus_configured_a_basic_host(role):
+    return role
+
+@when('the host is provisioned')
 def the_host_is_provisioned():
     return
 
-@then("all basic services are running")
-def all_basic_services_are_running():
-    pass
+@then('all basic services are running')
+def all_basic_services_are_running(role):
+    assert (role in serviceConfigs)
+    for service in serviceConfigs[role]:
+        assert host.get_host("local://").service(service).is_running, f'Service "{service}" is not running'
